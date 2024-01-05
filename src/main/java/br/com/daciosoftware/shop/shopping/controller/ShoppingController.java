acd2cc40250463c56cdb518c98f463ee9eea3eec
Dácio Braga <fdacio@gmail.com>
2024-01-05 20:41:36 -0300

    refatoracao para persistir vendas

--- src/main/java/br/com/daciosoftware/shop/shopping/controller/ShoppingController.java
+++ src/main/java/br/com/daciosoftware/shop/shopping/controller/ShoppingController.java
@@ -3,12 +3,17 @@ package br.com.daciosoftware.shop.shopping.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.daciosoftware.shop.modelos.dto.ShopDTO;
 import br.com.daciosoftware.shop.shopping.service.ShoppingService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/shopping")
@@ -23,4 +28,9 @@ public class ShoppingController {
 		return shoppingService.findAll();
 	}
 	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ShopDTO save(@Valid @RequestBody ShopDTO shopDTO) {		
+		return shoppingService.save(shopDTO);
+	}
 }
--- src/main/java/br/com/daciosoftware/shop/shopping/service/ShoppingService.java
+++ src/main/java/br/com/daciosoftware/shop/shopping/service/ShoppingService.java
@@ -1,10 +1,12 @@
 package br.com.daciosoftware.shop.shopping.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.daciosoftware.shop.modelos.dto.ShopDTO;
 import br.com.daciosoftware.shop.modelos.entity.Shop;
@@ -24,6 +26,17 @@ public class ShoppingService {
 				.stream()
 				.map(ShopDTO::convert)
 				.collect(Collectors.toList());
-		
 	}
+	
+	@Transactional
+	public ShopDTO save(ShopDTO shopDTO) {
+		shopRepository.deleteAll();
+		Float total = shopDTO.getItens().stream().map(i->i.getPreco()*i.getQuantidade()).reduce((float)0, Float::sum);
+		shopDTO.setData(LocalDateTime.now());
+		shopDTO.setTotal(total);
+		Shop shop = Shop.convert(shopDTO); 
+		shopRepository.save(shop);
+		return ShopDTO.convert(shop);
+	}
+
 }
