fff025d77c9fff428ee7a44bfaf5d93a493a8faf
Dácio Braga <fdacio@gmail.com>
2024-05-10 01:23:53 -0300

    refatoracao report pdf resumo de vendas

--- src/main/java/br/com/daciosoftware/shop/shopping/controller/ShoppingReportController.java
+++ src/main/java/br/com/daciosoftware/shop/shopping/controller/ShoppingReportController.java
@@ -1,17 +1,25 @@
 package br.com.daciosoftware.shop.shopping.controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.itextpdf.text.DocumentException;
+
 import br.com.daciosoftware.shop.modelos.dto.shopping.ShopDTO;
-import br.com.daciosoftware.shop.modelos.dto.shopping.ShopReportDTO;
 import br.com.daciosoftware.shop.shopping.service.ShoppingReportService;
 
 @RestController
@@ -38,7 +46,7 @@ public class ShoppingReportController {
 	}
 	
 	@GetMapping("/periodo")
-	public ShopReportDTO getReportByDate(
+	public ResponseEntity<?> getReportByDate(
 			@RequestParam(name = "dataInicio", required = true) 
 			@DateTimeFormat(pattern = "dd/MM/yyyy")
 			LocalDate dataInicio, 
@@ -46,6 +54,21 @@ public class ShoppingReportController {
 			@DateTimeFormat(pattern = "dd/MM/yyyy")
 			LocalDate dataFim) 
 	{
-		return shoppingReportService.getReportByDate(dataInicio, dataFim);
+		
+		try {
+			
+	        ByteArrayOutputStream pdfStream = shoppingReportService.getReportByDate(dataInicio, dataFim);
+	        HttpHeaders headers = new HttpHeaders();
+	        headers.setContentType(MediaType.APPLICATION_PDF);
+	        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=shop-resume.pdf");
+	        headers.setContentLength(pdfStream.size());
+	        
+	        return new ResponseEntity<>(pdfStream.toByteArray(), headers, HttpStatus.OK);
+	        
+		} catch (DocumentException | IOException | URISyntaxException e) {
+			e.printStackTrace();
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).body(e.getMessage());
+		}
+		
 	}
 }
--- src/main/java/br/com/daciosoftware/shop/shopping/service/ShoppingReportService.java
+++ src/main/java/br/com/daciosoftware/shop/shopping/service/ShoppingReportService.java
@@ -1,11 +1,30 @@
 package br.com.daciosoftware.shop.shopping.service;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Font.FontFamily;
+import com.itextpdf.text.Font.FontStyle;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import br.com.daciosoftware.shop.modelos.dto.shopping.ShopDTO;
 import br.com.daciosoftware.shop.modelos.dto.shopping.ShopReportDTO;
@@ -14,6 +33,8 @@ import br.com.daciosoftware.shop.shopping.repository.ShoppingReportRepositoryImp
 @Service
 public class ShoppingReportService {
 
+	private static int SCALE_PERC_LOGO = 20;
+	
 	@Autowired
 	private ShoppingReportRepositoryImpl shoppingReportRepository;
 	
@@ -23,9 +44,90 @@ public class ShoppingReportService {
 				.stream()
 				.map(ShopDTO::convert)
 				.collect(Collectors.toList());
+		
 	}
 	
-	public ShopReportDTO getReportByDate(LocalDate dataInicio, LocalDate dataFim) {
-		return shoppingReportRepository.getReportByDate(dataInicio, dataFim);
+	public ByteArrayOutputStream getReportByDate(LocalDate dataInicio, LocalDate dataFim) throws DocumentException, MalformedURLException, URISyntaxException, IOException {
+		
+		ShopReportDTO shopReportDTO = shoppingReportRepository.getReportByDate(dataInicio, dataFim);
+		
+		Document document = new Document();
+		document.setMargins(20, 20, 30, 30);
+	
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		
+		PdfWriter.getInstance(document, outputStream);
+		
+		document.open();
+		
+		addHeaderReport(document, "Resumo de Vendas");
+		
+		Font fontPeriodo = new Font(FontFamily.HELVETICA, 14, FontStyle.BOLD.ordinal());
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		String periodo = String.format("Período: %s à %s", dataInicio.format(dtf), dataFim.format(dtf));
+		document.add(new Phrase(periodo, fontPeriodo));
+		
+		addRowsReportByDate(document, shopReportDTO);
+		
+		document.close();
+		
+		return outputStream;
 	}
+	
+	private void addHeaderReport(Document document, String title) throws URISyntaxException, MalformedURLException, IOException, DocumentException {
+
+		Path path = Paths.get(ClassLoader.getSystemResource("static/images/logo.png").toURI());
+		Image img = Image.getInstance(path.toAbsolutePath().toString());
+		img.scalePercent(SCALE_PERC_LOGO);
+		
+		PdfPTable tableHeader = new PdfPTable(3);
+		tableHeader.setWidthPercentage(100);
+		float widths[] = {15, 70, 15};
+		tableHeader.setWidths(widths);
+		
+		PdfPCell pdfPCellImg = new PdfPCell(img);
+		pdfPCellImg.setBorderWidth(0);
+
+		Font fontTitle = new Font(FontFamily.HELVETICA, 18, FontStyle.BOLD.ordinal());
+		PdfPCell pdfPCellTitulo = new PdfPCell(new Phrase(title, fontTitle));
+		pdfPCellTitulo.setBorderWidth(0);
+		pdfPCellTitulo.setHorizontalAlignment(Element.ALIGN_CENTER);
+		pdfPCellTitulo.setVerticalAlignment(Element.ALIGN_MIDDLE);
+		
+		tableHeader.addCell(pdfPCellImg);
+		tableHeader.addCell(pdfPCellTitulo);
+		PdfPCell pdfPCell = new PdfPCell();
+		pdfPCell.setBorderWidth(0);
+		tableHeader.addCell(pdfPCell);
+		
+		document.add(tableHeader);
+
+	}
+
+	private void addRowsReportByDate(Document document, ShopReportDTO shopReportDTO) throws DocumentException {
+
+		PdfPTable table = new PdfPTable(2);
+		table.setWidthPercentage(100);
+		float widths[] = {40, 60};
+		table.setWidths(widths);	
+		
+		table.addCell("Quantidade de vendas no período:");
+		PdfPCell pdfPCellCount = new PdfPCell(new Phrase(String.format("%d", shopReportDTO.getCount())));
+		pdfPCellCount.setHorizontalAlignment(Element.ALIGN_RIGHT);
+		table.addCell(pdfPCellCount);
+		
+		table.addCell("Total de vendas no período:");
+		PdfPCell pdfPCellTotal = new PdfPCell(new Phrase(String.format("R$ %,.2f", shopReportDTO.getTotal())));
+		pdfPCellTotal.setHorizontalAlignment(Element.ALIGN_RIGHT);
+		table.addCell(pdfPCellTotal);
+	
+		table.addCell("Valor médio de vendas no período:");
+		PdfPCell pdfPCellMean = new PdfPCell(new Phrase(String.format("R$ %,.2f", shopReportDTO.getMean())));
+		pdfPCellMean.setHorizontalAlignment(Element.ALIGN_RIGHT);
+		table.addCell(pdfPCellMean);
+		
+		document.add(table);
+
+	}
+
 }
